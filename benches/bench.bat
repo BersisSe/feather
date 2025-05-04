50c59a1e9c9ffdffe35910cc4f0c2e9f29b99f0f
tigerros <aurel.leonard.danel@gmail.com>
2025-05-04 23:05:25 +0200

    fix bench scripts

--- benches/bench.bat
+++ benches/bench.bat
@@ -1,4 +1,4 @@
 cd ..
-docker build -f benches.Dockerfile -t %1-bench --build framework=%1 .
+docker build -f benches.Dockerfile -t %1-bench --build-arg framework=%1 .
 docker run --rm -d -p 3000:3000 %1-bench
 docker run --rm ghcr.io/william-yeh/wrk -t10 -c400 -d10s http://host.docker.internal:3000/
